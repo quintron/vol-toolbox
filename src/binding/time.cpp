a76353901ed443a74992043fbacafd5781e80f7b
quintron <quintron>
2021-02-26 15:24:26 +0100

    add python datetime binder

---
+++ src/binding/time.cpp
@@ -0,0 +1,61 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/chrono.h>
+
+#include <voltlbx/time_utils.h>
+
+
+namespace pybind11 {
+    namespace detail {
+
+        template <> class type_caster<chrono::DateTime> {
+        public:
+
+            bool load(handle src, bool) {
+                using namespace std::chrono;
+
+                // Lazy initialise the PyDateTime import
+                if (!PyDateTimeAPI) { PyDateTime_IMPORT; }
+
+                if (!src) return false;
+
+                unsigned year, month, day, hour, minute, sec;
+
+                if (PyDateTime_Check(src.ptr())) {
+                    sec = PyDateTime_DATE_GET_SECOND(src.ptr());
+                    minute = PyDateTime_DATE_GET_MINUTE(src.ptr());
+                    hour = PyDateTime_DATE_GET_HOUR(src.ptr());
+                    day = PyDateTime_GET_DAY(src.ptr());
+                    month = PyDateTime_GET_MONTH(src.ptr());
+                    year = PyDateTime_GET_YEAR(src.ptr());
+                }
+                else if (PyDate_Check(src.ptr())) {
+                    sec = 0;
+                    minute = 0;
+                    hour = 0;
+                    day = PyDateTime_GET_DAY(src.ptr());
+                    month = PyDateTime_GET_MONTH(src.ptr());
+                    year = PyDateTime_GET_YEAR(src.ptr());
+                }
+                else return false;
+
+                value = voltlbx::utc_datetime(year, month, day, hour, minute, sec);
+                return true;
+            }
+
+            static handle cast(const chrono::DateTime& src, return_value_policy /* policy */, handle /* parent */) {
+                using namespace std::chrono;
+
+                // Lazy initialise the PyDateTime import
+                if (!PyDateTimeAPI) { PyDateTime_IMPORT; }
+
+                auto [year, month, day] = voltlbx::get_year_month_day(src);
+                auto[hour, minute, sec] = voltlbx::get_hour_minute_second(src);
+
+                return PyDateTime_FromDateAndTime(year, month, day, hour, minute, sec, 0);
+            }
+            PYBIND11_TYPE_CASTER(chrono::DateTime, _("datetime.datetime"));
+        };
+
+
+    }
+}
--- src/binding/voltoolbox.cpp
+++ src/binding/voltoolbox.cpp
@@ -1,10 +1,20 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <pybind11/chrono.h>
+
 #include <voltlbx/smile.h>
+#include "time.cpp"
 
 namespace py = pybind11;
 using namespace voltlbx;
+
+namespace voltlbx_bind
+{
+    auto check_datetime(const chrono::DateTime& d)
+    {
+        return d;
+    }
+}
+
 
 PYBIND11_MODULE(_voltoolbox, m) {
 
@@ -14,13 +24,20 @@ PYBIND11_MODULE(_voltoolbox, m) {
     m.attr("__version__") = "dev";
 #endif
 
+    m.def("check_datetime", &voltlbx_bind::check_datetime, "A datetime test function");
+
+
     py::class_<Smile>(m, "Smile")
         .def("vol", &Smile::vol, py::arg("x"))
         .def("density_ratio", &Smile::density_ratio, py::arg("x"))
         .def("pseudo_local_vol", &Smile::pseudo_local_vol, py::arg("x"));
 
+
     py::class_<CubicSplineSmile, Smile>(m, "CubicSplineSmile")
         .def(py::init<double, double, std::vector<double>, std::vector<double>>(),
              py::arg("time_to_maturity"), py::arg("atf_vol"), py::arg("zs"), py::arg("vol_ratios"));
 
+
 }
+
+
