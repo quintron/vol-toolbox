f65e4093762a97e84d2d4581634fa53a4c5e6d90
quintron <quintron>
2021-06-21 20:58:49 +0200

    improve forward fit algo

--- notebooks/fit_utils.py
+++ notebooks/fit_utils.py
@@ -1,6 +1,67 @@
 import datetime as dt
+import numpy as np
+
+from typing import Tuple
+from voltoolbox import longest_increasing_subsequence
+from voltoolbox.fit.option_quotes import OptionQuoteSlice, QuoteSlice
 
 
 def act365_time(t0: dt.datetime, t1: dt.datetime):
     delta = t1 - t0
     return (delta.days + delta.seconds / 86400.0) / 365.0
+
+
+def filter_quotes(quote_slice: OptionQuoteSlice,
+                  fwd_bracket: Tuple[float, float],
+                  *, discounted_output_quotes: bool=True
+                 ) -> OptionQuoteSlice:
+    """Return discounted option quotes, outlier have been removed.
+    """
+    fwd_min, fwd_max = fwd_bracket
+    assert(fwd_max >= fwd_min)
+
+    discount = quote_slice.discount
+    out_discount = 1.0 if discounted_output_quotes else discount
+
+    # Prepare call slice
+    call_sl = quote_slice.call
+    call_ks = np.array(call_sl.strikes)
+    call_bid = np.array(call_sl.bids) / discount
+    call_ask = np.array(call_sl.asks) / discount
+
+    call_sanity_filter = (call_bid > 0.0) & (call_ask > call_bid)
+    call_ks = call_ks[call_sanity_filter]
+    call_bid = call_bid[call_sanity_filter]
+    call_ask = call_ask[call_sanity_filter]
+    call_mid = 0.5 * (call_ask + call_bid)
+
+    put_intrinsic_filter = call_mid > np.maximum(0.0, fwd_min - call_ks)
+    inc_subseq = longest_increasing_subsequence(-call_mid[put_intrinsic_filter])
+    filt_call_sl = QuoteSlice(tuple(call_ks[put_intrinsic_filter][inc_subseq]),
+                              tuple(out_discount * call_bid[put_intrinsic_filter][inc_subseq]),
+                              tuple(out_discount * call_ask[put_intrinsic_filter][inc_subseq]))
+
+    # Prepare put slice
+    put_sl = quote_slice.put
+    put_ks = np.array(put_sl.strikes)
+    put_bid = np.array(put_sl.bids) / discount
+    put_ask = np.array(put_sl.asks) / discount
+
+    put_sanity_filter = (put_bid > 0.0) & (put_ask > put_bid)
+    put_ks = put_ks[put_sanity_filter]
+    put_bid = put_bid[put_sanity_filter]
+    put_ask = put_ask[put_sanity_filter]
+
+    put_mid = 0.5 * (put_ask + put_bid)
+
+    call_intrinsic_filter = put_mid > np.maximum(0.0, put_ks - fwd_max)
+    inc_subseq = longest_increasing_subsequence(put_mid[call_intrinsic_filter])
+    filt_put_sl = QuoteSlice(tuple(put_ks[call_intrinsic_filter][inc_subseq]),
+                             tuple(out_discount * put_bid[call_intrinsic_filter][inc_subseq]),
+                             tuple(out_discount * put_ask[call_intrinsic_filter][inc_subseq]))
+
+    return OptionQuoteSlice(quote_slice.symbol, 
+                            quote_slice.expiry,
+                            discount,
+                            filt_call_sl, 
+                            filt_put_sl)
--- notebooks/forward_fit.py
+++ notebooks/forward_fit.py
@@ -1,20 +1,20 @@
 import math
 import numpy as np
 import datetime as dt
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 
 from voltoolbox import BusinessTimeMeasure, longest_increasing_subsequence, bs_implied_volatility
 from voltoolbox.fit.option_quotes import OptionSnapshot, OptionQuoteSlice, QuoteSlice
 from voltoolbox.calendar import nyse_calendar
 
-from fit_utils import act365_time
+from fit_utils import act365_time, filter_quotes
 
 
-def prepare_quotes_for_fit(quote_slice: OptionQuoteSlice, 
+def prepare_quotes_for_fit(quote_slice: OptionQuoteSlice,
                            pricing_date: dt.datetime,
-                           discount : float,
                            spot_prior: float,
-                           *, yield_threshold=0.10):
+                           *, yield_threshold=0.10
+                           ) -> OptionQuoteSlice:
     """Return discounted option quotes, outlier have been removed.
     """
 
@@ -22,37 +22,7 @@ def prepare_quotes_for_fit(quote_slice: OptionQuoteSlice,
     ratio_min = min(math.exp(- time_to_mat * yield_threshold), (1.0 - yield_threshold))
     ratio_max = math.exp(time_to_mat * yield_threshold)
     
-    call_sl = quote_slice.call
-    call_ks = np.array(call_sl.strikes)
-    call_bid = np.array(call_sl.bids) / discount
-    call_ask = np.array(call_sl.asks) / discount
-    call_mid = 0.5 * (call_ask + call_bid)
-
-    put_plus_fwd = call_mid + call_ks 
-    filter_ = put_plus_fwd > spot_prior * ratio_min
-    inc_subseq = longest_increasing_subsequence(put_plus_fwd[filter_])
-    filt_call_sl = QuoteSlice(tuple(call_ks[filter_][inc_subseq]),
-                              tuple(call_bid[filter_][inc_subseq]),
-                              tuple(call_ask[filter_][inc_subseq]))
-
-
-    put_sl = quote_slice.put
-    put_ks = np.array(put_sl.strikes)
-    put_bid = np.array(put_sl.bids) / discount
-    put_ask = np.array(put_sl.asks) / discount
-    put_mid = 0.5 * (put_ask + put_bid)
-
-    filter_ = put_mid - put_ks > - spot_prior * ratio_max
-    inc_subseq = longest_increasing_subsequence(put_mid[filter_])
-    filt_put_sl = QuoteSlice(tuple(put_ks[filter_][inc_subseq]),
-                             tuple(put_bid[filter_][inc_subseq]),
-                             tuple(put_ask[filter_][inc_subseq]))
-
-    return OptionQuoteSlice(quote_slice.symbol, 
-                            quote_slice.expiry,
-                            1.0,
-                            filt_call_sl, 
-                            filt_put_sl)
+    return filter_quotes(quote_slice, (spot_prior * ratio_min, spot_prior * ratio_max), discounted_output_quotes = True)
 
 
 class OptionKernelRegression:
@@ -66,12 +36,15 @@ class OptionKernelRegression:
         put_bids = np.array(opt_slice.put.bids)
         put_asks = np.array(opt_slice.put.asks)
         
+        self.ks = np.array(opt_slice.call.strikes + opt_slice.put.strikes)
         self.put_target = np.concatenate(
                             (0.5 * (call_bids + call_asks) + call_ks, 
                              0.5 * (put_bids + put_asks)
-                            ), axis=0)
-        self.ks = np.array(opt_slice.call.strikes + opt_slice.put.strikes)
-        
+                            ), axis=0)        
+        self.put_err = np.concatenate((0.5 * (call_asks - call_bids), 
+                                       0.5 * (put_asks - put_bids)
+                                     ), axis=0)
+
         self.smoothing = 1.0e-10 #Numerical parameter 
 
     def call_put_premium(self, k: float, kernel_width: float):
@@ -98,7 +71,7 @@ class OptionKernelRegression:
                                  self.ks - k, 
                                  np.vectorize(convex)((self.ks / k - 1.0) / kernel_width)])
 
-        ws = np.exp(-0.5 * ((self.ks / k - 1.0) / kernel_width) **2)
+        ws = np.sqrt(np.exp(-0.5 * ((self.ks / k - 1.0) / kernel_width) **2) / self.put_err)
         b_ws = np.column_stack([ws] * 4) 
 
         var = np.matmul((basis * b_ws).T, basis * b_ws)
@@ -115,47 +88,45 @@ class OptionKernelRegression:
 def fit_forward_curve(quotes: OptionSnapshot, 
                       box_spread: float) -> Dict[dt.datetime, float]:
 
-    business_time = BusinessTimeMeasure(nyse_calendar(), 0.5, 252.0)
     pricing_dt = quotes.time_stamp
-    
+
     previous_fit_fwd = quotes.ref_spot
     forwards = {}
     for quote_sl in quotes.slices:        
 
         if (quote_sl.expiry < pricing_dt):
             continue
-        
-        discount = quote_sl.discount * math.exp(-box_spread * act365_time(pricing_dt, quote_sl.expiry))
+
+        t = act365_time(pricing_dt, quote_sl.expiry)
+        box_discount = quote_sl.discount * math.exp(-box_spread * t)
+        quote_sl = OptionQuoteSlice(quote_sl.symbol,
+                                    quote_sl.expiry,
+                                    box_discount,
+                                    quote_sl.call,
+                                    quote_sl.put)
         quote_sl = prepare_quotes_for_fit(quote_sl,
                                           pricing_dt,
-                                          discount,
-                                          quotes.ref_spot,
-                                          yield_threshold=0.05)
-
-        t = business_time.distance(pricing_dt, quote_sl.expiry)
-        dev = 0.15 * np.sqrt(t)
-        kernel_width = 0.5 * dev
-    
+                                          previous_fit_fwd,
+                                          yield_threshold=0.01)
+        
+        # Raw estimation of atm spot volatility
+        DEFAULT_VOL = 0.15
         opt_kernel = OptionKernelRegression(quote_sl)
-        c, p = opt_kernel.call_put_premium(quotes.ref_spot, kernel_width)
+        c, p = opt_kernel.call_put_premium(quotes.ref_spot, 0.5 * DEFAULT_VOL * np.sqrt(t))
         raw_fwd = c - p + quotes.ref_spot
         raw_vol = 0.5 * (bs_implied_volatility(raw_fwd, quotes.ref_spot, p, t, -1.0)
                          + bs_implied_volatility(raw_fwd, quotes.ref_spot, c, t, 1.0))
 
         dev = raw_vol * np.sqrt(t)
-        kernel_width = 0.25 * dev
-        ks = raw_fwd * np.exp(dev * np.linspace(-1.0, 1.0, 11))
+        KERNEL_NB_DEV_WIDTH = 0.10
+        kernel_width = KERNEL_NB_DEV_WIDTH * dev
+        ks = raw_fwd * np.exp(dev * np.linspace(-0.5, 0.5, 11))
         estimated_fwds = []
         for k in ks:
-            try :
-                c, p = opt_kernel.call_put_premium(k, kernel_width)
-                estimated_fwds.append(c - p + k)
-            except:
-                pass
-
+            c, p = opt_kernel.call_put_premium(k, kernel_width)
+            estimated_fwds.append(c - p + k)
         forward = np.median(estimated_fwds)
-        
-        
+
         forwards[quote_sl.expiry] = forward
         previous_fit_fwd = forward
 
